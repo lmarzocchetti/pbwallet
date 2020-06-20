8bc9b5b7148ea1daccb290d09d0de575c74a1290
lmarzocchetti <lorenzomarzocchetti@gmail.com>
2020-06-20 17:56:22 +0200

    Aggiunto LoginPanel

---
+++ app/src/main/java/com/example/pbwallet/LoginPanel.java
@@ -0,0 +1,11 @@
+package com.example.pbwallet;
+
+import android.content.Context;
+import android.view.View;
+
+public class LoginPanel extends View {
+
+    public LoginPanel(Context context) {
+        super(context);
+    }
+}
