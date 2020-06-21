0060c4f34771548439f5d551af6b56ec7aa9de8f
lmarzocchetti <lorenzomarzocchetti@gmail.com>
2020-06-21 18:55:09 +0200

    Added a BottomnavBar with 3 fragments

--- app/src/main/java/com/example/pbwallet/HomePanel.java
+++ app/src/main/java/com/example/pbwallet/HomePanel.java
@@ -2,24 +2,53 @@ package com.example.pbwallet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class HomePanel extends Activity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomePanel extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepanel);
-        TextView name = (TextView) findViewById(R.id.textView4);
-        TextView surname = (TextView) findViewById(R.id.textView5);
-        String strname = "Giuseppe";
-        String strsurname = "Pultrone";
-        ChangeName(name,surname,strname,strsurname);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_frameLayout, new HomeFragment()).commit();
+        }
+
+        BottomNavigationView navbar = findViewById(R.id.nav_bar);
+        navbar.setOnNavigationItemSelectedListener(navigationlistener);
+
+
     }
 
-    private void ChangeName(TextView name, TextView surname, String strname, String strsurname){
-        name.setText(strname);
-        surname.setText(strsurname);
-    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationlistener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selected = null;
+                    switch(item.getItemId()) {
+                        case R.id.nav_home:
+                            selected = new HomeFragment();
+                            break;
 
+                        case R.id.nav_fund:
+                            selected = new FundsFragment();
+                            break;
+
+                        case R.id.nav_stats:
+                            selected = new StatsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_frameLayout, selected).commit();
+                    return true;
+                }
+            };
 }
--- app/src/main/java/com/example/pbwallet/LoginPanel.java
+++ app/src/main/java/com/example/pbwallet/LoginPanel.java
@@ -2,12 +2,13 @@ package com.example.pbwallet;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class LoginPanel extends Activity{
+    EditText username, passwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +16,22 @@ public class LoginPanel extends Activity{
         setContentView(R.layout.activity_loginpanel);
 
         Button enter = findViewById(R.id.login_button);
+        username = findViewById(R.id.editTextTextPersonName);
+        passwd = findViewById(R.id.editTextTextPassword);
 
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent homepage = new Intent(LoginPanel.this ,LoginPanel.class);
-                startActivity(homepage);
-                finish();
+                Intent homepage = new Intent(LoginPanel.this ,HomePanel.class);
+                if(checkEnter(username.getText().toString(), passwd.getText().toString())) {
+                    startActivity(homepage);
+                    finish();
+                }
             }
         });
+    }
+
+    protected boolean checkEnter(String username, String passwd) {
+        return true;
     }
 }
