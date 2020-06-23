2ab0036199c72395476dffa98534593e56d20bf4
lmarzocchetti <lorenzomarzocchetti@gmail.com>
2020-06-23 11:56:48 +0200

    Finished Signin panel

--- app/src/main/java/com/example/pbwallet/MainActivity.java
+++ app/src/main/java/com/example/pbwallet/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Intent loginpage = new Intent(MainActivity.this, LoginPanel.class);
         startActivity(loginpage);
     }
